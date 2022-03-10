9e9fd59fbe60a5203d44fb672626318011e589aa
POPCharlyn <1540912102@qq.com>
2022-03-10 14:24:16 +0800

    react官方教程井字棋完结

--- src/index.js
+++ src/index.js
@@ -12,80 +12,16 @@ function Square( props ) {
     );
 }
 
-// class Square extends React.Component {
-//     // 添加构造函数，初始化state
-//     // constructor( props ) {
-//     //     super( props ); // 在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头
-//     //     this.state = {
-//     //         value: null,
-//     //     };
-//     // }
-//
-//     render() {
-//         return (
-//             <button
-//                 className="square"
-//                 onClick={ () => this.props.onClick( { value: 'X' } ) }
-//             >
-//                 { this.props.value }
-//             </button>
-//         );
-//     }
-// }
-
 class Board extends React.Component {
-    // 为 Board 组件添加构造函数，将 Board 组件的初始状态设置为长度为 9 的空值数组：
-    // constructor( props ) {
-    //     super( props );
-    //     this.state = {
-    //         squares: Array( 9 ).fill( null ),
-    //         // xIsNext（布尔值）
-    //         xIsNext: true,
-    //     };
-    // }
-
-    handleClick( i ) {
-        // https://zh.javascript.info/array-methods#slice
-        // 使用 .slice() 函数对 squares 数组进行拷贝，而非直接修改现有的数组
-        const history = this.state.history;
-        const current = history[ history.length - 1 ];
-        const squares = this.state.squares.slice();
-
-        if ( calculateWinner( squares ) || squares[ i ] ) {
-            return;
-        }
-        squares[ i ] = this.state.xIsNext ? 'X' : 'O'; // 实现 “X” 和 “O” 轮流落子
-        this.setState( {
-            history: history.concat( [ { // concat() 方法创建一个新数组，并不会改变原数组
-                squares: squares,
-            } ] ),
-            xIsNext: !this.state.xIsNext,
-        } );
-    }
-
     renderSquare( i ) {
         return (
-            <Square value={ this.state.squares[ i ] }
-                    onClick={ () => this.handleClick( i ) }
+            <Square value={ this.props.squares[ i ] }
+                    onClick={ () => this.props.onClick( i ) }
             />
         );
     }
 
     render() {
-        // const winner = calculateWinner( this.state.squares );
-        // console.log( winner );
-        // let status; // 定义 status
-        //
-        // if ( winner ) {
-        //     status = 'Winner:' + winner;
-        //     console.log( winner );
-        // } else {
-        //     status = 'Next player:' + ( this.state.xIsNext ? 'X' : 'O' );
-        // }
-        // console.log( status );
-        // console.log( winner );
-        //const status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O' );
-
         return (
             <div>
                 <div className="board-row">
@@ -117,21 +53,69 @@ class Game extends React.Component {
             history: [ {
                 squares: Array( 9 ).fill( null ),
             } ],
+            stepNumber: 0,
             xIsNext: true,
         };
+    }
+
+    handleClick( i ) {
+        // https://zh.javascript.info/array-methods#slice
+        // 使用 .slice() 函数对 squares 数组进行拷贝，而非直接修改现有的数组
+        const history = this.state.history.slice( 0, this.state.stepNumber + 1 );
+        const current = history[ history.length - 1 ];
+        const squares = current.squares.slice();
+
+        if ( calculateWinner( squares ) || squares[ i ] ) {
+            return;
+        }
+        squares[ i ] = this.state.xIsNext ? 'X' : 'O'; // 实现 “X” 和 “O” 轮流落子
+        this.setState( {
+                           history: history.concat( [ { // concat() 方法创建一个新数组，并不会改变原数组
+                               squares: squares,
+                           } ] ),
+                           stepNumber: history.length,
+                           xIsNext: !this.state.xIsNext,
+                       } );
+    }
+
+    jumpTo( step ) {
+        this.setState( {
+                           stepNumber: step,
+                           // 每当我们落下一颗新棋子的时候，我们需要调用 this.setState
+                           // 并传入参数 stepNumber: history.length，以更新 stepNumber
+                           xIsNext: (
+                                        step % 2
+                                    ) === 0, // 当状态 stepNumber 是偶数时，我们把 xIsNext 设为 true
+                       } );
     }
 
     render() {
         // 使用最新一次历史记录来确定并展示游戏的状态
         const history = this.state.history;
-        const current = history[ history.length - 1 ];
+        // 将代码从始终根据最后一次移动渲染修改为根据当前 stepNumber 渲染
+        const current = history[ this.state.stepNumber ];
         const winner = calculateWinner( current.squares );
+
+        const moves = history.map( ( step, move ) => {
+            const desc = move ?
+                         'Go to move #' + move :
+                         'Go to game start';
+            return (
+                <li key={ move }>
+                    <button
+                        onClick={ () => this.jumpTo( move ) }>{ desc }</button>
+                </li>
+            );
+        } );
+
         let status;
 
         if ( winner ) {
             status = 'Winner:' + winner;
         } else {
-            status = 'Next player:' + ( this.state.xIsNext ? 'X' : 'O' );
+            status = 'Next player:' + (
+                this.state.xIsNext ? 'X' : 'O'
+            );
         }
 
         return (
@@ -139,12 +123,12 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board
                         squares={ current.squares }
-                        onClick={ ( i ) => this.handleClick( i ) }
+                        onClick={ i => this.handleClick( i ) }
                     />
                 </div>
                 <div className="game-info">
                     <div>{ status }</div>
-                    <ol>{/* TODO */ }</ol>
+                    <ol>{ moves }</ol>
                 </div>
             </div>
         );
@@ -153,10 +137,8 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-    <Game/>,
-    document.getElementById( 'root' )
-);
+ReactDOM.render( <Game/>, document.getElementById( 'root' ) );
+
 
 // calculateWinner 函数，显示游戏的结果来判定游戏结束
 function calculateWinner( squares ) {
@@ -172,7 +154,7 @@ function calculateWinner( squares ) {
     ];
     for ( let i = 0; i < lines.length; i++ ) {
         const [ a, b, c ] = lines[ i ];
-        if ( squares[ a ] && squares[ a ] === [ b ] && squares[ a ] === squares[ c ] ) {
+        if ( squares[ a ] && squares[ a ] === squares[ b ] && squares[ a ] === squares[ c ] ) {
             return squares[ a ];
         }
     }
